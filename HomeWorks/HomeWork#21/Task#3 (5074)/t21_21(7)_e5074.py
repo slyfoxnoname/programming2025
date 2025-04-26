e17e6b23c5500982428b5e57aca80f9ac3725b23
Egor Orlik <egororlik27@knu.ua>
2025-04-26 17:29:03 +0300

    New HomeWork#21 (26.05.25)

---
+++ HomeWorks/HomeWork#21/Task#3 (5074)/t21_21(7)_e5074.py
@@ -0,0 +1,13 @@
+def main():
+    with open("input.txt") as f:
+        n, m = map(int, f.readline().split())  # ← читаємо з файлу
+        degree = [0] * n
+        for _ in range(m):
+            a, b = map(int, f.readline().split())
+            degree[a - 1] += 1
+            degree[b - 1] += 1
+        for d in degree:
+            print(d)
+
+if __name__ == '__main__':
+    main()
