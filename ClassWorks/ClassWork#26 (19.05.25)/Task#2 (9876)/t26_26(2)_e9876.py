89a8123c1a5ce2d4bcaa5ce94499fd443d965945
Egor Orlik <egororlik27@knu.ua>
2025-05-19 16:24:11 +0300

    New Task#2 in ClassWork#26

--- ClassWorks/ClassWork#26 (19.05.25)/Task#2 (9876)/t26_26(2)_e9876.py
+++ ClassWorks/ClassWork#26 (19.05.25)/Task#2 (9876)/t26_26(2)_e9876.py
@@ -1,48 +1,52 @@
 def hamming_distance(s1, s2):
     return sum(a != b for a, b in zip(s1, s2))
 
-def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])
-    return parent[x]
-
-def union(parent, rank, x, y):
-    xroot = find(parent, x)
-    yroot = find(parent, y)
-    if xroot == yroot:
-        return False
-    if rank[xroot] < rank[yroot]:
-        parent[xroot] = yroot
-    else:
-        parent[yroot] = xroot
-        if rank[xroot] == rank[yroot]:
-            rank[xroot] += 1
-    return True
 if __name__ == '__main__':
     with open('input.txt') as f:
         n, k = map(int, f.readline().split())
         dna = [f.readline().strip() for _ in range(n)]
 
-        edges = []
-        for i in range(n):
-            for j in range(i + 1, n):
-                d = hamming_distance(dna[i], dna[j])
-                edges.append((d, i, j))
+    # Побудова повного графа: для кожної вершини зберігаємо суміжні ребра
+    graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            d = hamming_distance(dna[i], dna[j])
+            graph[i].append((d, j))
+            graph[j].append((d, i))
 
-        edges.sort()
+    # Ініціалізація для алгоритму Прима
+    visited = [False] * n
+    mst_weight = 0
+    mst_edges = []
 
-        parent = list(range(n))
-        rank = [0] * n
-        mst_weight = 0
-        mst_edges = []
+    # Пріоритетна черга (містить пари (вага, вершина, звідки))
+    heap = [(0, 0, -1)]  # Починаємо з вершини 0
 
-        for weight, u, v in edges:
-            if union(parent, rank, u, v):
-                mst_weight += weight
-                mst_edges.append((u, v))
-                if len(mst_edges) == n - 1:
-                    break
+    while heap and len(mst_edges) < n - 1:
+        # Вибираємо найкоротніше ребро
+        min_weight = float('inf')
+        min_index = -1
+        for i in range(len(heap)):
+            if heap[i][0] < min_weight:
+                min_weight = heap[i][0]
+                min_index = i
+        weight, u, from_v = heap[min_index]
+        heap[min_index] = heap[-1]
+        heap.pop()
 
-        print(mst_weight)
-        for u, v in mst_edges:
-            print(u, v)
+        if visited[u]:
+            continue
+
+        visited[u] = True
+        mst_weight += weight
+        if from_v != -1:
+            mst_edges.append((from_v, u))
+
+        for w, v in graph[u]:
+            if not visited[v]:
+                heap.append((w, v, u))
+
+    # Вивід результату
+    print(mst_weight)
+    for u, v in mst_edges:
+        print(u, v)
